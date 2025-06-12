02232c27549fff14e452c5ced40d483b09c3c573
tristanjones-student <tristanjones@myboisestate.edu>
2025-06-12 15:05:48 -0600

    updated backend with CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://54.186.136.170/:3000/', 'http://localhost3000'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
